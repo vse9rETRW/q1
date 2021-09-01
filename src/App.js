f9d89a4d3935f37c90cbf5d7143aca934f036687
henry.lch <henry.lch@link8tech.tw>
2021-09-01 14:50:44 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect} from 'react';
 
 function App() {
+  useEffect(() =>
+      fetch('/api/authentication'), [])
   return (
     <div className="App">
       <header className="App-header">
